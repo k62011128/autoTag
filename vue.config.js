b99b752bfd093b695395c332aa9aebcf89ecc088
kanami <jdzhuzhuoning@163.com>
2022-12-29 12:13:33 +0800

    2022.12.29

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  configureWebpack: {
+    resolve: {
+      extensions: ['.js', '.vue', '.json', '.ts'] // 后缀名
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/] // ts-loader
+          }
+        }
+      ]
+    }
+  },
 })
